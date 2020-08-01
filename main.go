4e2a8450eefe7809ca8607023c269bc42238f387
talha khan <talhak2111@gmail.com>
2020-08-01 23:22:42 +0530

    Added .gitignore

--- main.go
+++ main.go
@@ -6,16 +6,29 @@ import (
 	"os"
 	"io/ioutil"
 	)
+
 func check(err error){
+	if err!=nil{
 	panic(err)
 	}
-
-
-func main(){
-	fileName:=os.Args[1]
-	content,err:=ioutil.ReadFile(fileName);
-	check(err);
-	fmt.Println(content)
 	}
 
-	
+func readFile(name string)([]byte){
+	content,err:=ioutil.ReadFile(name);
+	check(err);
+	return content
+	}
+func main(){
+	if len(os.Args)==1{
+		fmt.Println("Please Provide a valid filename to parse")
+		fmt.Println("For e.g $./csv filename_of_file_to_be_parsed")
+	}else if len(os.Args)>1{
+		parse(readFile(string(os.Args[1])))
+		}
+		}
+
+
+func parse(arr[]byte){
+	fmt.Println(string(arr));
+	}
+
